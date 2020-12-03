0b4b008f64993810c5e9dbb91ddeb416f5950921
vAndresJara <victor.jarae@gmail.com>
2020-12-03 08:04:45 -0300

    Se implementa eliminar desde la base de datos.

--- app/src/main/java/com/example/fitness_app/Insumos_act.java
+++ app/src/main/java/com/example/fitness_app/Insumos_act.java
@@ -3,6 +3,7 @@ package com.example.fitness_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -52,4 +53,63 @@ public class Insumos_act extends AppCompatActivity {
         }
 
     }
+
+    public void MostrarInsumnos(View v)
+    {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "fichero", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String codigo = edCodigo.getText().toString();
+
+        if(!codigo.isEmpty())
+        {
+            //Clase cursor me permite recorrer filas
+            Cursor fila = bd.rawQuery("SELECT nombre, precio, stock FROM insumos where codigo="+codigo,null);
+
+            if(fila.moveToFirst())
+            {
+                edNombre.setText(fila.getString(0));
+                edPrecio.setText(fila.getString(1));
+                edStock.setText(fila.getString(2));
+            }
+            else{
+                Toast.makeText(this,"El producto no existe",Toast.LENGTH_LONG).show();
+            }
+
+        }
+        else
+        {
+            Toast.makeText(this, "Debe ingresar el codigo del insumo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void EliminarInsumnos(View v)
+    {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "fichero", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+
+        String codigo = edCodigo.getText().toString();
+
+        if(!codigo.isEmpty())
+        {
+
+            bd.delete("insumos","codigo="+codigo,null);//Eliminar el campo según el codigo
+            bd.close();
+            Toast.makeText(this,"Has eliminado el insumo",Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toast.makeText(this, "Debe ingresar el codigo del insumo", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+    public void ActualizarInsumnos(View v)
+    {
+
+    }
+
+
 }
